46f63b0a96f168ccb7a4e44bb22f9b57d05945f1
vermamohit13 <b20215@students.iitmandi.ac.in>
2022-08-29 13:09:29 +0530

    Added Files & ignored some

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+#include "functions.h"
+#include <bits/stdc++.h>
+using namespace std;
+int factorial(int n){
+    if(n < 0)
+    {
+        cout<<"Enter a positive no.\n";
+        return -1;
+    }
+    else if(n!=1){
+	return(n * factorial(n-1));
+    }
+    else return 1;
+}
