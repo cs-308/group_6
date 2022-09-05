380098922bb7c928d45b177a17b4a8cb1181ff49
vermamohit13 <b20215@students.iitmandi.ac.in>
2022-09-05 12:12:10 +0530

    Reverted printf to cout

--- factorial.cpp
+++ factorial.cpp
@@ -1,10 +1,10 @@
-#include "functions.h"
+\#include "functions.h"
 #include <bits/stdc++.h>
 using namespace std;
 int factorial(int n){
     if(n < 0)
     {
-        cout<<"Enter a positive no.\n";
+        cout<<"Enter a positive no. as an input \n";
         return -1;
     }
     else if(n!=1){
@@ -12,4 +12,5 @@ int factorial(int n){
     }
     else return 1;
 }
+// Added changes for negative number
 // Vishwas Made a change
